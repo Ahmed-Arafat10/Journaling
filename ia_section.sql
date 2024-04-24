c7f04f1af99d40be633f829a8c66072f40b04e59
Ahmed_Arafat10 <ahmedxarafat0101@gmail.com>
2024-04-24 23:16:20 +0200

    adding .sql file for Database

---
+++ ia_section.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2024 at 03:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ia_section`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `date`
+--
+
+CREATE TABLE `date` (
+  `ID` int(11) NOT NULL,
+  `Date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `date`
+--
+
+INSERT INTO `date` (`ID`, `Date`) VALUES
+(1, '2024-04-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diary`
+--
+
+CREATE TABLE `diary` (
+  `ID` int(11) NOT NULL,
+  `Date_ID` int(11) NOT NULL,
+  `Diary` longtext NOT NULL,
+  `User_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `to-do-list`
+--
+
+CREATE TABLE `to-do-list` (
+  `ID` int(11) NOT NULL,
+  `Date_ID` int(11) NOT NULL,
+  `Note` text NOT NULL,
+  `Is_Done` bit(1) NOT NULL,
+  `User_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `to-do-list`
+--
+
+INSERT INTO `to-do-list` (`ID`, `Date_ID`, `Note`, `Is_Done`, `User_ID`) VALUES
+(2, 1, 'Hello BIS', b'0', 27),
+(3, 1, 'xyz', b'0', 27);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`ID`, `Name`, `Password`, `Email`) VALUES
+(26, 'AHMED', '$2y$10$v3TK509OaKo8TIiFas9Bn.A2ZvgAYjwXUk6H3iXXhd5Ro7qw2xiim', 'ahmed@gmail.com'),
+(27, 'ahmed_arafat', '$2y$10$w.2HEDZ5aWgMgAM.a0RGzuW15YKcgSI1XzF3JdzLWmaBEOjsXbBSu', 'admin1@admin.com'),
+(28, 'Marwa', '$2y$10$HlxEwEdgaOS0x7JXEOqLIuv8kSZq3MIqLOUiikrvZ4AVdtWmLhQ3a', 'marwa@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `date`
+--
+ALTER TABLE `date`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `diary`
+--
+ALTER TABLE `diary`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Date_ID` (`Date_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `to-do-list`
+--
+ALTER TABLE `to-do-list`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Date_ID` (`Date_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `date`
+--
+ALTER TABLE `date`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `diary`
+--
+ALTER TABLE `diary`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `to-do-list`
+--
+ALTER TABLE `to-do-list`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `diary`
+--
+ALTER TABLE `diary`
+  ADD CONSTRAINT `diary_ibfk_1` FOREIGN KEY (`Date_ID`) REFERENCES `date` (`ID`),
+  ADD CONSTRAINT `diary_ibfk_2` FOREIGN KEY (`User_ID`) REFERENCES `user` (`ID`);
+
+--
+-- Constraints for table `to-do-list`
+--
+ALTER TABLE `to-do-list`
+  ADD CONSTRAINT `to-do-list_ibfk_1` FOREIGN KEY (`Date_ID`) REFERENCES `date` (`ID`),
+  ADD CONSTRAINT `to-do-list_ibfk_2` FOREIGN KEY (`User_ID`) REFERENCES `user` (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
